e947149bc2b94c2533c715bcf394be7bf5d7ea3e
Xyriz Tan <xcptan@gmail.com>
2021-03-09 16:31:28 +0800

    First commit

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -16,13 +16,23 @@
 package com.example.androiddevchallenge
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -39,9 +49,120 @@ class MainActivity : AppCompatActivity() {
 // Start building your app here!
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+
+    Surface(
+        color = MaterialTheme.colors.background,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text("Sai's Jetpack Compose Countdown Timer :D", modifier = Modifier.padding(24.dp))
+            val countDownSecondsValue = remember { mutableStateOf(0) }
+            val isTimerRunning = remember { mutableStateOf(false) }
+            val textFieldEditable = derivedStateOf { !isTimerRunning.value }
+            val textFieldValue = remember { mutableStateOf("00h00m00s") }
+            val textFieldInput = remember { mutableStateOf("") }
+            TextField(
+                modifier = Modifier.padding(24.dp),
+                enabled = textFieldEditable.value,
+                value = textFieldValue.value,
+                onValueChange = { text ->
+                    // backspace registered
+                    if (text.length < 9) {
+                        if (textFieldInput.value.isNotEmpty()) {
+                            textFieldInput.value =
+                                textFieldInput.value.substring(0, textFieldInput.value.length - 1)
+                        }
+                        // shift digits to the right
+                    } else {
+                        textFieldInput.value = textFieldInput.value + text.last()
+                        // shift digits to the left only until if the first digit is zero
+                    }
+                    val rawString = String.format(
+                        "%06d", try {
+                            textFieldInput.value.toInt()
+                        } catch (e: NumberFormatException) {
+                            0
+                        }
+                    )
+                    val sb = StringBuilder(rawString)
+                    sb.insert(2, 'h')
+                    sb.insert(5, 'm')
+                    sb.insert(8, 's')
+                    val formattedString = sb.toString()
+                    textFieldValue.value = formattedString
+
+                    val nums = formattedString.split("[hms]".toRegex())
+                    val hours = nums[0].toInt()
+                    val minutes = nums[1].toInt()
+                    val seconds = nums[2].toInt()
+                    countDownSecondsValue.value = hours * 3600 + minutes * 60 + seconds
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Row {
+                val startButtonToggleText = remember { mutableStateOf("Start") }
+                var timer = remember { mutableStateOf<CountDownTimer?>(null) }
+                Button(
+                    modifier = Modifier.padding(16.dp),
+                    onClick = {
+                        if (!isTimerRunning.value) {
+                            timer.value =
+                                object :
+                                    CountDownTimer(countDownSecondsValue.value * 1000L, 1000L) {
+                                    override fun onTick(millisUntilFinished: Long) {
+                                        countDownSecondsValue.value--
+                                        textFieldValue.value =
+                                            countDownSecondsValue.value.toCountDownFormat()
+                                    }
+
+                                    override fun onFinish() {
+                                        isTimerRunning.value = false
+                                    }
+                                }.start()
+                            isTimerRunning.value = true
+                        } else {
+                            timer.value?.cancel()
+                            timer.value = null
+                            isTimerRunning.value = false
+                        }
+                    }) {
+                    Text(text = if (!isTimerRunning.value) "Start" else "Stop")
+                }
+                Button(modifier = Modifier.padding(16.dp), onClick = {
+                    timer.value?.cancel()
+                    timer.value = null
+                    isTimerRunning.value = false
+
+                    countDownSecondsValue.value = 0
+                    textFieldValue.value = "00h00m00s"
+                    textFieldInput.value = ""
+                }) {
+                    Text(text = "Reset")
+                }
+            }
+        }
     }
+}
+
+fun Int.toCountDownFormat(): String {
+    val hours = this / (60 * 60)
+    val minutes = this / 60 % 60
+    val seconds = this % 60
+
+    return "${String.format("%02d", hours)}h${
+        String.format(
+            "%02d",
+            minutes
+        )
+    }m${String.format("%02d", seconds)}s"
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
